e0d6d862a9774a90a48f67ee8185dc6b35afe00b
yui-knk <spiketeika@gmail.com>
2019-03-15 22:39:39 +0900

    Update pp contents

--- mrblib/pg_inspect/query.rb
+++ mrblib/pg_inspect/query.rb
@@ -4,9 +4,27 @@ class PgInspect
       pp.add_line "Query: #{command_type}(id: #{queryId})"
 
       pp.with_indent_inc do
+        pp.add_line "rtable (# #{rtable.count})"
+        pp.with_indent_inc do
+          rtable.each do |r|
+            r._pretty_print(pp)
+          end
+        end
+      end
+
+      pp.with_indent_inc do
         pp.add_line "jointree"
         pp.with_indent_inc do
           jointree._pretty_print(pp)
+        end
+      end
+
+      pp.with_indent_inc do
+        pp.add_line "target_list (# #{target_list.count})"
+        pp.with_indent_inc do
+          target_list.each do |t|
+            t._pretty_print(pp)
+          end
         end
       end
     end
--- mrblib/pg_inspect/range_tbl_entry.rb
+++ mrblib/pg_inspect/range_tbl_entry.rb
@@ -1,6 +1,7 @@
 class PgInspect
   class RangeTblEntry
     def _pretty_print(pp)
+      pp.add_line "RangeTblEntry: rtekind: #{rtekind}, relid: #{relid}, relkind: #{relkind}"
     end
   end
 end
